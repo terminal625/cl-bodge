fcb609b8cb990370414c378355a975b52b46052e
Pavel Korolev <dev@borodust.org>
2018-03-01 22:33:44 +0300

    Remove unnecessary explicit host argument

--- demo/demo.lisp
+++ demo/demo.lisp
@@ -4,6 +4,27 @@
 (cl:in-package :cl-bodge.demo)
 
 
-(defclass demo (ge:generic-system)
+(defclass demo (ge:enableable ge:generic-system)
   ()
   (:default-initargs :depends-on '(ge:host-system ge:graphics-system ge:audio-system)))
+
+
+(defun render (demo)
+  (declare (ignore demo))
+  (gl:clear-color 1.0 1.0 1.0 1.0)
+  (gl:clear :color-buffer)
+  (ge:swap-buffers))
+
+(defmethod ge:initialize-system :after ((this demo))
+  (ge:run (flow:serially
+           (ge:for-host ()
+             (setf (ge:swap-interval) 1))
+           (ge:loop-flow
+            (flow:serially
+             (ge:for-graphics ()
+               (render this)))
+            (lambda () (ge:enabledp this))))))
+
+
+(defun run ()
+  (ge:startup `(:engine (:systems (demo)))))
