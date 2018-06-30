4b61d6a4744b2ca2642cda287b21b9963f9f064d
Pavel Korolev <dev@borodust.org>
2018-06-30 14:47:42 +0300

    Simplify phong shader implementation

--- demo/cases/3d-physics/demo.glsl
+++ demo/cases/3d-physics/demo.glsl
@@ -7,14 +7,19 @@ struct DemoValue {
 };
 
 uniform DemoValue value;
+uniform mat4 MVP;
 
 #ifdef VERTEX_SHADER
 
 in vec3 vPosition;
+in vec3 vNormal;
+
+out vec3 normal;
 
 void main () {
-  gl_Position = vec4(vPosition, 1.0);
-  gl_PointSize = 2.0f;
+  gl_Position = MVP * vec4(vPosition, 1.0);
+  gl_PointSize = (sin(value.value) + 1.0) * 2.0f * (1 + cos(vPosition.x));
+  normal = vNormal;
 }
 
 #endif
@@ -22,9 +27,16 @@ void main () {
 #ifdef FRAGMENT_SHADER
 
 out vec4 fColor;
+in vec3 normal;
+
+uniform PhongMaterial material;
+uniform PhongPointLight light;
+uniform mat4 view;
+uniform vec3 diffuseColor;
 
 void main() {
-  fColor = vec4(0.5 + sin(value.value) / 2, tg(value.value), 0.5 + cos(value.value) / 2, 1.0);
+  fColor = vec4(calcPhongReflection(light, material, normalize(normal), vec3(0.0, 0.0, 1.0),
+                                    diffuseColor, 1.0, MVP), 1.0);
 }
 
 #endif
